2502ce3d57dac7b675eb5128c3575caf16a2269f
Mustafa1906 <72093036+Mustafa1906@users.noreply.github.com>
2020-10-07 17:31:52 +0100

    i update it my

---
+++ src/test/java/EU3_Practice.java
@@ -0,0 +1,15 @@
+public class EU3_Practice {
+
+    public static void main(String[] args) {
+        System.out.println("EU3 is opening pull");
+        //commit you chance
+        //with nice commit message
+        //push it back to the github
+        //observe the change
+        //from the your Github repo
+        //click on compare
+        //create a pull request
+        //to asl original repo owner
+        //to merge your change
+    }
+}
